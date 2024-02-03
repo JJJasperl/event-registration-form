85a11e021302f9b45fd317818b3e2a5edb0fbfb0
Jasper Liu <jasperl@berkeley.edu>
2024-02-02 23:24:44 -0800

    finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,155 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
+import React from "react";
 
 export default function Home() {
+  const [essay, setText] = useState(""); 
+  // world count that represents the cound of essay
+  const wordCount = essay.split(/\s+/).filter(Boolean).length; 
+  const handleEssayChange = (e: { target: { value: React.SetStateAction<string>; }; }) => { 
+    setText(e.target.value); 
+  };
+
+  // Require fields check
+  const handleSubmit = (e: {
+    target: any; preventDefault: () => void; 
+    }) => {
+    e.preventDefault();
+
+    // Check if essay is within the character limit
+    if (essay.length > 1000) {
+      alert("Essay must be 1000 characters or less");
+      return;
+    } else if (essay.length === 0) {
+      alert("Essay must not be empty");
+      return;
+    }
+
+    const fullName = e.target.elements.namedItem("fullName") as HTMLInputElement;
+    const university = e.target.elements.namedItem("university") as HTMLInputElement; 
+    const major = e.target.elements.namedItem("major") as HTMLInputElement; 
+    const graduationDate = e.target.elements.namedItem("graduationDate") as HTMLInputElement;
+    const gender = e.target.elements.namedItem("gender") as HTMLInputElement;
+    const country = e.target.elements.namedItem("country") as HTMLInputElement;
+
+
+    if (fullName.value.trim() === "") {
+      alert("Please fill out your full name");
+      return;
+    }
+    if (university.value.trim() === "") {
+      alert("Please fill out your university");
+      return;
+    }
+    if (major.value.trim() === "") {
+      alert("Please fill out your major");
+      return;
+    }
+    if (graduationDate.value.trim() === "") {
+      alert("Please fill out your graduation date");
+      return;
+    }
+    if (gender.value === "") {
+      alert("Please fill out your gender");
+      return;
+    }
+    if (country.value.trim() === "") {
+      alert("Please fill out your country");
+      return;
+    }
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h1 className="text-2xl font-semibold mb-4">Cal Hacks 11.0 Registration</h1>
+        <form onSubmit={handleSubmit}>
+          {/* full name */}
+          <div className="mb-4">
+            <label htmlFor="fullName" className="block text-sm font-medium text-gray-600">
+              Full Name
+            </label>
+            <input type="text" id="fullName" name="fullName" className="mt-1 p-2 w-full border rounded" required/>
+          </div>
+          {/* University */}
+          <div className="mb-4">
+            <label htmlFor="university" className="block text-sm font-medium text-gray-600">
+              University
+            </label>
+            <input type="text" id="university" name="university" className="mt-1 p-2 w-full border rounded" required/>
+          </div>
+          {/* major */}
+          <div className="mb-4">
+            <label htmlFor="major" className="block text-sm font-medium text-gray-600">
+              Major
+            </label>
+            <input type="text" id="major" name="major" className="mt-1 p-2 w-full border rounded" required/>
+          </div>
+          {/* graduation date */}
+          <div className="mb-4">
+            <label htmlFor="graduationDate" className="block text-sm font-medium text-gray-600">
+              Graduation Date
+            </label>
+            <input type="date" id="graduationDate" name="graduationDate" className="mt-1 p-2 w-full border rounded" required/>
+          </div>
+          {/* gender */}
+          <div className="mb-4">
+            <label htmlFor="graduationDate" className="block text-sm font-medium text-gray-600">
+              Gender
+              <select id="gender" name="gender" className="mt-1 p-2 w-full border rounded" defaultValue="" required>
+              <option value="" disabled>Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+              <option value="dontWantToSay">Don't want to say</option>
+              </select>
+            </label>
+          </div>
+          {/* Country of Residence */}
+          <div className="mb-4">
+            <label htmlFor="Country of Residence" className="block text-sm font-medium text-gray-600">
+            Country of Residence
+            </label>
+            <input type="text" id="Country" name="Country" className="mt-1 p-2 w-full border rounded" required />
+          </div>
+          {/* Essay */}
+          <div className="mb-4">
+            <label htmlFor="essay" className="block text-sm font-medium text-gray-600">
+            Why are you interested in attending Cal Hacks 11.0? What are you interested in
+            building? (1000 character maximum)
+            </label>
+            <textarea
+              id="essay"
+              name="essay"
+              className="mt-1 p-2 w-full border rounded"
+              rows={5}
+              maxLength={1000}
+              onChange={handleEssayChange} 
+              required
+            ></textarea>
+          </div>
+
+          {/* Display the word count */}
+          <div className="text-sm text-gray-600 mb-4">
+            Word Count: {wordCount}/1000
+          </div>
+          
+          {/* Checkbox for checking if the participant is 18+ years of age */}
+          <div className="mb-4">
+            <label>
+              <input type="checkbox" className="mr-2" required />
+              Are you 18 years old or older?
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            Register
+          </button>
+        </form>
       </div>
     </main>
   );
